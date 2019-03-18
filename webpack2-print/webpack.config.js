58e7626e47ec75737b3d9db4e4eba3e390f75da9
zora <zora.xiao@anker.com>
2019-03-18 11:24:42 +0800

    modify  clean-webpack-plugin err

--- webpack2-print/webpack.config.js
+++ webpack2-print/webpack.config.js
@@ -9,16 +9,19 @@ module.exports = {
         print: './src/print.js'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
         //自动生成index.html文件、、、、、
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        //该插件会根据output路径自动识别输出文件，并删除历史文件，只保留最新构建
+        new CleanWebpackPlugin(),
+
     ],
     output: {
         //文件名必须是动态生成的，不然会文件名冲突
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        //会根据输出路径自动创建新的文件夹，HtmlWebpackPlugin，CleanWebpackPlugin俩插件会根据输出路径自动选择相应文件进行构建
+        path: path.resolve(__dirname, 'dist1')
     },
 }
 ;
