d0e2b7f720901b613c8537c05ad98d81905d4384
zora <zora.xiao@anker.com>
2019-03-26 13:18:35 +0800

    d3

---
+++ d3-test/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import BarChart from "./D3-test";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <BarChart/>
+            </div>
+        );
+    }
+}
+
+export default App;
